bc7438cce04dc882b3fcc49ad92d44e4f3cc66a5
carlosandre-dev <andre.tjlba@gmail.com>
2022-12-26 15:08:13 -0300

    Primeiro Componente no Angular

---
+++ fundamentos-angular/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-primeiro-componente",
+  template: "<h2>Primeiro Componente em Angular</h2>",
+  styles: ["h2 { color: red }"]
+})
+export class PrimeiroComponenteComponent {}
